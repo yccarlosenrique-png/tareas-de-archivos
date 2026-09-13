196dd2c9fa99e16b3239173b160e5357aa25f2e8
yccarlosenrique-png <yc.carlosenrique@gmail.com>
2026-09-12 19:56:22 -0500

    Add files via upload

---
+++ function2.js
@@ -0,0 +1,4 @@
+function stringaurl(nombre){
+    return "http://www." + nombre + ".com";
+}
+console.log(stringaurl("carlos"));
---
+++ function3.js
@@ -0,0 +1,5 @@
+function stringAadmiracion(frase){
+    return "¡" + frase + "!";
+}
+
+console.log(stringAadmiracion("los malos y los buenos no se mezclaran"));
