67ab24faf76b05305b88fe8d2f0584350a826185
yccarlosenrique-png <yc.carlosenrique@gmail.com>
2026-09-12 19:55:21 -0500

    Create function.js

---
+++ function.js
@@ -0,0 +1,8 @@
+function convertirpulgadasacentimetro (pulgadas) {
+    let centimetroporpulgada = 2.54;
+    return pulgadas * centimetroporpulgada;
+
+}
+
+console.log(convertirpulgadasacentimetro(4));
+console.log(convertirpulgadasacentimetro(3));
